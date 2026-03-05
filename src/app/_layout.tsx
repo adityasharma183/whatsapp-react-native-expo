6dd2a437652788abcbf330e9d396dca03647905f
Aditya Sharma <adityaa.sharma9757@gmail.com>
2026-03-05 12:18:08 +0530

    initial expo setup

---
+++ src/app/_layout.tsx
@@ -0,0 +1,21 @@
+import { View, Text } from 'react-native'
+import React, { useEffect } from 'react'
+import { Stack } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync()
+
+const RootNavigation = () => {
+  useEffect(()=>{
+    setTimeout(()=>{
+      SplashScreen.hideAsync();
+    },2000)
+  },[])
+  return (
+    <Stack>
+     <Stack.Screen name='index'/>
+    </Stack>
+  )
+}
+
+export default RootNavigation
